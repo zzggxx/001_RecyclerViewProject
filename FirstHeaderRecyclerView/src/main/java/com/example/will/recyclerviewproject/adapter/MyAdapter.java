8a44416109f5fe62e3cd3529aefa2f1a53f02c32
Will <will.g.zhou@magugi.com>
2018-07-18 15:12:11 +0800

    使用adapter方式添加头部

---
+++ FirstHeaderRecyclerView/src/main/java/com/example/will/recyclerviewproject/adapter/MyAdapter.java
@@ -0,0 +1,158 @@
+package com.example.will.recyclerviewproject.adapter;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.will.recyclerviewproject.R;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+
+    private ArrayList<String> mDatas = new ArrayList<>();
+
+    private View mHeaderView;
+
+    private OnItemClickListener mListener;
+
+    public void setOnItemClickListener(OnItemClickListener li) {
+        mListener = li;
+    }
+
+    /**
+     * 注意这里是局部刷新.在某位置上边添加数据并且伴有动画
+     *
+     * @param headerView
+     */
+    public void addHeaderView(View headerView) {
+        mHeaderView = headerView;
+        notifyItemInserted(0);
+    }
+
+    public View getHeaderView() {
+        return mHeaderView;
+    }
+
+    public void addDatas(ArrayList<String> datas) {
+        mDatas.addAll(datas);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (position == 0 && mHeaderView != null) {
+            return TYPE_HEADER;
+        } else {
+            return TYPE_NORMAL;
+        }
+    }
+
+    /**
+     * 使用了相同Holder,在holder里边做了手脚.
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new Holder(mHeaderView);
+        } else {
+            View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            return new Holder(layout);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+
+        if (getItemViewType(position) == TYPE_HEADER)
+            return;
+
+//        getRealPosition.newApi for me.
+        final int pos = getRealPosition(viewHolder);
+        final String data = mDatas.get(pos);
+
+        if (viewHolder instanceof Holder) {
+            ((Holder) viewHolder).text.setText(data);
+            if (mListener == null) return;
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mListener.onItemClick(pos, data);
+                }
+            });
+        }
+    }
+
+    public int getRealPosition(RecyclerView.ViewHolder holder) {
+        int position = holder.getLayoutPosition();
+        return mHeaderView == null ? position : position - 1;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHeaderView == null ? mDatas.size() : mDatas.size() + 1;
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+
+        TextView text;
+
+        public Holder(View itemView) {
+            super(itemView);
+            if (itemView == mHeaderView) return;
+            text = itemView.findViewById(R.id.text);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, String data);
+    }
+
+    /**
+     * GridlayoutManager
+     *
+     * @param recyclerView
+     */
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if (manager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = ((GridLayoutManager) manager);
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    return getItemViewType(position) == TYPE_HEADER ? gridManager.getSpanCount() : 1;
+                }
+            });
+        }
+    }
+
+    /**
+     * StaggeredGridLayoutManager适应,和GridlayoutManager处理方法不一样的.
+     *
+     * @param holder
+     */
+    @Override
+    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+        if (lp != null && lp instanceof StaggeredGridLayoutManager.LayoutParams) {
+            StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
+            p.setFullSpan(holder.getLayoutPosition() == 0);
+        }
+    }
+}
---
+++ FirstHeaderRecyclerView/src/main/java/com/example/will/recyclerviewproject/base/adapter/BaseAdapter.java
@@ -0,0 +1,158 @@
+package com.example.will.recyclerviewproject.base.adapter;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public abstract class BaseAdapter<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+
+    private ArrayList<T> mDatas = new ArrayList<>();
+
+    private View mHeaderView;
+
+    /**
+     * 注意这里是局部刷新.在某位置上边添加数据并且伴有动画
+     *
+     * @param headerView
+     */
+    public void addHeaderView(View headerView) {
+        mHeaderView = headerView;
+        notifyItemInserted(0);
+    }
+
+    public View getHeaderView() {
+        return mHeaderView;
+    }
+
+    public void addDatas(ArrayList<T> datas) {
+        mDatas.addAll(datas);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (position == 0 && mHeaderView != null) {
+            return TYPE_HEADER;
+        } else {
+            return TYPE_NORMAL;
+        }
+    }
+
+    /**
+     * onCreateViewHolder(parent, viewType);使用了封装者模式.
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new Holder(mHeaderView);
+        } else {
+            return onCreateView(parent, viewType);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+
+        if (getItemViewType(position) == TYPE_HEADER)
+            return;
+
+//        getRealPosition.newApi for me.
+        final int pos = getRealPosition(viewHolder);
+        final T data = mDatas.get(pos);
+        onBindData(viewHolder, pos, data);
+
+        if (mListener != null) {
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mListener.onItemClick(pos, data);
+                }
+            });
+        }
+    }
+
+    public int getRealPosition(RecyclerView.ViewHolder holder) {
+        int position = holder.getLayoutPosition();
+        return mHeaderView == null ? position : position - 1;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHeaderView == null ? mDatas.size() : mDatas.size() + 1;
+    }
+
+    /**
+     * 抽象了布局和绑定数据.
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    public abstract RecyclerView.ViewHolder onCreateView(ViewGroup parent, final int viewType);
+
+    public abstract void onBindData(RecyclerView.ViewHolder viewHolder, int RealPosition, T data);
+
+    public class Holder extends RecyclerView.ViewHolder {
+        public Holder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public OnItemClickListener mListener;
+
+    public void setOnItemClickListener(OnItemClickListener li) {
+        mListener = li;
+    }
+
+    public interface OnItemClickListener<T> {
+        void onItemClick(int position, T data);
+    }
+
+    /**
+     * GridlayoutManager
+     *
+     * @param recyclerView
+     */
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if (manager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = ((GridLayoutManager) manager);
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    return getItemViewType(position) == TYPE_HEADER ? gridManager.getSpanCount() : 1;
+                }
+            });
+        }
+    }
+
+    /**
+     * StaggeredGridLayoutManager适应,和GridlayoutManager处理方法不一样的.
+     *
+     * @param holder
+     */
+    @Override
+    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+        if (lp != null && lp instanceof StaggeredGridLayoutManager.LayoutParams) {
+            StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
+            p.setFullSpan(holder.getLayoutPosition() == 0);
+        }
+    }
+}
